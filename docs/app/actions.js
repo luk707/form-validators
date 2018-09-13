1a5b965b5f7a45aad4dfed38237529a9033e1ee1
Luke Harris <lukeharris01170@gmail.com>
2018-09-13 21:48:29 +0100

    Remove old docs stuff

--- docs/app/actions.js
+++
@@ -1,6 +0,0 @@
-export const toggleNavbar = () => ({ type: "TOGGLE_NAVBAR" });
-
-export const selectRelease = path => ({
-  type: "SELECT_RELEASE",
-  payload: path
-});
--- docs/app/selectors.js
+++
@@ -1,9 +0,0 @@
-export const navbar = state => state.navbar;
-
-export const navbarIsOpen = state => navbar(state).open;
-
-export const releases = state => state.releases.releases;
-
-export const loadingReleases = state => state.releases.loading;
-
-export const selectedRelease = state => state.selectedRelease.path;
