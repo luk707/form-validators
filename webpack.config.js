e8acc2f5472b14aaaec9f485afa4931b8ddf0bf3
Luke Harris <lukeharris01170@gmail.com>
2018-09-14 20:18:32 +0100

    Shuffle structure around

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require("webpack");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ["*", ".js"]
+  },
+  entry: "./docs/index.js",
+  output: {
+    path: __dirname + "/.docs",
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: "./docs",
+    hot: true
+  }
+};
