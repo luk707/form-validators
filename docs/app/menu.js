4f3c433a875cf19c80663d206a555b35e7183fd8
Luke Harris <lukeharris01170@gmail.com>
2018-09-15 00:21:05 +0100

    Add search icon to filter

--- docs/app/menu.js
+++ docs/app/menu.js
@@ -10,14 +10,19 @@ class MenuComponent extends Component {
   render() {
     return (
       <Fragment>
-        <input
-          className="input is-rounded"
-          type="text"
-          placeholder="Filter"
-          value={this.state.filter}
-          onChange={this.handleFilter}
-          style={{ marginBottom: "1rem" }}
-        />
+        <p className="control has-icons-right">
+          <input
+            className="input is-rounded"
+            type="text"
+            placeholder="Filter"
+            value={this.state.filter}
+            onChange={this.handleFilter}
+            style={{ marginBottom: "1rem" }}
+          />
+          <span className="icon is-right">
+            <i className="fal fa-search" />
+          </span>
+        </p>
         <Menu>
           <MenuLabel>General</MenuLabel>
           <MenuList>
